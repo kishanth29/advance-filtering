5579634852f6eb93f123cc93e630afcdb1885958
kishanth29 <139776257+kishanth29@users.noreply.github.com>
2024-04-13 14:52:16 +0530

    overaal begun

---
+++ src/Products/Product.js
@@ -0,0 +1,324 @@
+import {AiFillStar } from 'react-icons/ai';
+import { BsBagHeartFill } from "react-icons/bs";
+import "./Product.css";
+
+ function Product() {
+  return <>
+  <section className="card-container">
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    <section className="card">
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" 
+      alt="shoe"
+      className='card-img'/>
+      <div className="card-details">
+        <h3 className="card-title">shoe</h3>
+        <section className="card-reviews">
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <AiFillStar className='rating-star'/>
+        <span className="total-reviews">4</span>
+        </section>
+      <section className="card-price">
+        <div className="price">
+          <del>Rs300</del> 200
+        </div>
+        <div className="bag">
+          <BsBagHeartFill />
+        </div>
+      </section>
+
+      </div>
+    </section>
+    
+  </section>
+  </>
+}
+export default Product;
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return 
+  <>
+  <div>side bar</div>
+  
+  </>
+}
+export default Sidebar;
